6568451339056e0f1b9ebb882e47757e7bcf41d0
kostyaprozhenik <kostyaprozhenik@gmail.com>
2022-05-03 01:59:35 +0300

    feat: implement

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+
+  let currentNode = l;
+  while(currentNode.next) {
+    if (currentNode.next.value === k) {
+      currentNode.next = currentNode.next.next;
+    }
+    currentNode = currentNode.next;
+  }
+  return l;
 }
 
 module.exports = {
